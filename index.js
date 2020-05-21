657282fda57423a542752602da0fd3ad199de9c2
othman19t <othman19t@gmail.com>
2020-05-21 01:32:45 -0700

    Read-Write data asynchronously

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const fs = require("fs"); // require fileSystem module
 const writeTxt = fs.writeFileSync("./myFile.txt", "this is my text"); // write a text in myFile.txt
 const readTxt = fs.readFileSync("./myFile.txt", "utf-8"); // read the text from myFile.txt
 console.log(readTxt); // log readTxt which contain the text we read from myFile.txt
+
+// Write-Read data asynchronously
+fs.writeFile("./myFile.txt", "new data", "utf-8", (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  fs.readFile("./myFile.txt", "utf-8", (err, data) => {
+    console.log(data);
+  });
+});
