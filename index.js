918ed1a8b72f3922b32a31c304971f37dc4e166d
othman19t <othman19t@gmail.com>
2020-05-21 20:00:25 -0700

    #13 simple API

--- index.js
+++ index.js
@@ -13,21 +13,32 @@ fs.writeFile("./myFile.txt", "new data", "utf-8", (err) => {
   });
 });
 
+// read data to get it ready to send it to the browser when it is requested
+const data = fs.readFileSync("./myFile.json", "utf-8");
+const dataObject = JSON.parse(data);
 //setup an http server
 const http = require("http"); //require http module
 const server = http.createServer((req, res) => {
   //setup the login route and communicating with the browser
   const pathName = req.url;
-  if (pathName == "/login") {
-    return res.end("this is the login page");
+  if (pathName === "/login") {
+    res.end("this is the login page");
+    // setup API page
+  } else if (pathName === "/api") {
+    // sending header and json data
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    //sending header and status
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello world",
+    });
+    // sending html to the browser
+    res.end("<h1 >Page not found</h1>");
   }
-  //sending header and status
-  res.writeHead(404, {
-    "Content-type": "text/html",
-    "my-own-header": "hello world",
-  });
-  // sending html to the browser
-  res.end("<h1 >Page not found</h1>");
 });
 
 // listen to port 8000 to serve any http request on port 8000
