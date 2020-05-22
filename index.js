2aef6d85e9a4d38652b294371e94ce35411c0f22
othman19t <othman19t@gmail.com>
2020-05-21 22:21:07 -0700

    #15 filling the html template with the read data

--- index.js
+++ index.js
@@ -13,9 +13,29 @@ fs.writeFile("./myFile.txt", "new data", "utf-8", (err) => {
   });
 });
 
+//define function to replace the placeholders
+replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not_organic");
+  }
+  return output;
+};
 // read data to get it ready to send it to the browser when it is requested
+const tempOw = fs.readFileSync(`${__dirname}/templates/overview.html`, "utf-8");
+const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`, "utf-8");
+const tempPeo = fs.readFileSync(`${__dirname}/templates/product.html`, "utf-8");
+
 const data = fs.readFileSync("./myFile.json", "utf-8");
 const dataObject = JSON.parse(data);
+
 //setup an http server
 const http = require("http"); //require http module
 const server = http.createServer((req, res) => {
@@ -30,6 +50,19 @@ const server = http.createServer((req, res) => {
       "Content-type": "application/json",
     });
     res.end(data);
+    //setup overview route
+  } else if (pathName === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    //looping around the object data and replace the placeholders
+    const cardHtml = dataObject
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOw.replace("{%PRODUCT_CARDS%}", cardHtml);
+    res.end(output);
+    //setup product route
+  } else if (pathName === "/product") {
   } else {
     //sending header and status
     res.writeHead(404, {
