8fcce4b13d62414a0f5ec888c4cffd74de245f07
othman19t <othman19t@gmail.com>
2020-05-22 20:06:31 -0700

    #17 creating rplace module and fixing not-organic

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 const fs = require("fs"); // require fileSystem module
 const http = require("http"); //require http module
 const url = require("url"); //require url module
-//define function to replace the placeholders
-replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not_organic");
-  }
-  return output;
-};
+const replaceTemplate = require("./modules/replaceTemplate");
 // read data to get it ready to send it to the browser when it is requested
 const tempOw = fs.readFileSync(`${__dirname}/templates/overview.html`, "utf-8");
 const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`, "utf-8");
